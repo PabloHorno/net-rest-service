6da72e25ecfe4e8b24a87c6842296b2defc70825
Pablo Horno <pablojhorno@gmail.com>
2021-03-30 03:48:38 +0200

    Added Greetings methods and controller routes

---
+++ GreetingsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using net_rest_service.Model;
+
+namespace net_rest_service
+{
+    [Route("/greetings")]
+    [ApiController]
+    public class GreetingsController : ControllerBase
+    {
+        private int counter;
+
+        [HttpGet]
+        public Greetings GetGreetings(string name = "Wolrd!")
+        {
+            return new Greetings(counter++, name);
+        }
+    }
+}
---
+++ Model/Greetings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace net_rest_service.Model
+{
+    public class Greetings
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Greetings(int id, string name)
+        {
+            Id = id;
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+        }
+    }
+}
